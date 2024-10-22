a058c9ed99de7add4bd93969370ccaa5cb7c7ba9
Sathya Narayanan Gopu(UST <204198@ust.com>
2024-10-22 14:17:59 +0530

    changes

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,24 +6,19 @@ resource "aws_instance" "web" {
   vpc_security_group_ids       = [var.security_group_id]
   key_name                    = var.key_name
 
-  network_interface {
-    network_interface_id = aws_network_interface.vnet0.id
-    device_index         = 0
-  }
-
   tags = {
     Name = "web-instance"
   }
 }
 
-# Elastic IP and Network Interface
-resource "aws_network_interface" "vnet0" {
-  subnet_id       = var.subnet_id
-  private_ips     = [var.static_ip]
-  security_groups = [var.security_group_id]
-}
+# # Elastic IP and Network Interface
+# resource "aws_network_interface" "vnet0" {
+#   subnet_id       = var.subnet_id
+#   private_ips     = [var.static_ip]
+#   security_groups = [var.security_group_id]
+# }
 
-resource "aws_eip" "eip" {
-  network_interface = aws_network_interface.vnet0.id
-  domain               = "vpc"
-}
+# resource "aws_eip" "eip" {
+#   network_interface = aws_network_interface.vnet0.id
+#   domain               = "vpc"
+# }
